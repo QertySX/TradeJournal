9299f0010a669ea84720e14fe4d670629f6b4572
ArtemHirus <artem.girus212@gmail.com>
2025-08-28 15:16:15 +0300

    API для главной страницы

---
+++ _home/views.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, APIRouter, Request
+import logging
+from fastapi.templating import Jinja2Templates
+
+
+logging.basicConfig(level=logging.INFO)
+home_router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@home_router.get("/index")
+async def get_index(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request
+        })
+
+
+@home_router.get("/trades")
+async def get_index(request: Request):
+    return templates.TemplateResponse("trades.html", {
+        "request": request
+        })
+
