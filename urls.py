3a7eb7c96ff5be1dc65a3e7cb737625fe78b28ea
ArtemHirus <artem.girus212@gmail.com>
2025-10-14 16:00:24 +0300

    Зареєстровано роутер accounts

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from _home.views import home_router
 from _todo.views import todo_router
 from _trades.views import trades_router
+from _accounts.views import accounts_router
 
 
 app = FastAPI()
@@ -9,3 +10,4 @@ app = FastAPI()
 app.include_router(home_router)
 app.include_router(todo_router)
 app.include_router(trades_router)
+app.include_router(accounts_router)
