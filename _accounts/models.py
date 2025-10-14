387262e03ec7198ade170345dc821702fb1af9c8
ArtemHirus <artem.girus212@gmail.com>
2025-10-14 15:59:37 +0300

    Не значні зміни

--- _accounts/models.py
+++ _accounts/models.py
@@ -3,17 +3,19 @@ from sqlalchemy.orm import declarative_base, relationship, Mapped, mapped_column
 from datetime import datetime, date
 from database.base import Base
 
+from _trades.models import Trades
+
 class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     login: Mapped[str] = mapped_column(String(20), nullable=False)
-    password_hash: Mapped[str] = mapped_column(String(50), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(250), nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
 
 
-    trades: Mapped[list['Trades']] = relationship('Trades', back_populates='user')
+    trades: Mapped[list['Trades']] = relationship(Trades, back_populates='user')
 
     def __repr__(self):
         return f"User(login={self.login!r}, created_at={self.created_at!r})"
