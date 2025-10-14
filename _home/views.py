c66e17e1f7c1da70988a0adc21d96d64ff1e7e52
ArtemHirus <artem.girus212@gmail.com>
2025-10-14 15:58:03 +0300

    Не значні зміни

--- _home/views.py
+++ _home/views.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, APIRouter, Request
 import logging
 from fastapi.templating import Jinja2Templates
 
-
 logging.basicConfig(level=logging.INFO)
 home_router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -11,6 +10,6 @@ templates = Jinja2Templates(directory="templates")
 @home_router.get("/index")
 async def get_index(request: Request):
     return templates.TemplateResponse("index.html", {
-        "request": request
+        "request": request,
         })
 
